c35251736091a9a1e5a4c7e44f93ea6a8ab1244f
lenaruch <89196825388@mail.ru>
2022-12-09 22:09:20 +0600

    Add folders

---
+++ HomeWork_Hard/HWH_1/Program.cs
@@ -0,0 +1,41 @@
+﻿// Найти второй максимум 
+Console.Clear();
+
+Console.WriteLine("Введите числа: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int max1 = 0;
+int max2 = 0;
+while (n != 0)
+{
+    if (n > max1)
+    {
+        max2 = max1;
+        max1 = n;
+    }
+    else if (n > max2)
+    {
+        max2 = n;
+    }
+    n = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine($"Значение второго максимального: {max2}");
+
+
+// task dop
+// Console.Clear();
+// Console.Write("Введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine()), max1 = n, max2 = -1;
+// while (n != 0)
+// {
+//     n = Convert.ToInt32(Console.ReadLine());
+//     if (max1 < n)
+//     {
+//         max2 = max1;
+//         max1 = n;
+//     }
+//     else if (max2 < n)
+//     {
+//         max2 = n;
+//     }
+// }
+// Console.WriteLine(max2);
---
+++ Tasks/Task_15/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Clear();
+Console.Write("Введите день недели: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n < 1 || n > 7)
+{
+    Console.Write("Вы ошиблись!\nВведите день недели: ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+
+if (n == 6 || n == 7)
+    Console.WriteLine("Выходной");
+else
+    Console.WriteLine("На работу");
