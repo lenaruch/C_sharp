ccd05e841c1374ccea5163e1e67c66f0b281ff1d
lenaruch <89196825388@mail.ru>
2022-12-10 14:02:13 +0600

    add Homework

---
+++ HomeWork/Task_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Clear();
+
+Console.Write("Введите 1-ое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+if (m == n)
+    Console.Write("Числа равны");
+else if (m < n )
+    Console.Write($"Max: {n}, Min: {m}");
+else if (n < m )
+    Console.Write($"Max: {m}, Min: {n}");
---
+++ HomeWork/Task_4/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Clear();
+
+Console.Write("Введите 1-ое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (n >= m && n >= b)
+    Console.WriteLine($"Max: {n}");
+else if (m >= n && m >= b)
+    Console.WriteLine($"Max: {m}");
+else if (b >= n && b >= m)
+    Console.WriteLine($"Max: {b}");
