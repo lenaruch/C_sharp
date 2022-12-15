87d3ee248883800c9002ec6e888f584e539bcc54
lenaruch <89196825388@mail.ru>
2022-12-15 13:13:32 +0600

    Task 27 done

--- Seminars/Seminar_5/Program.cs
+++ Seminars/Seminar_5/Program.cs
@@ -1,7 +1,169 @@
-﻿// Здесь должна быть ссылка на запись семинара
+﻿// https://gbcdn.mrgcdn.ru/uploads/record/228462/attachment/d2c0aafa02245084a4563e4f88bdeb92.mp4
 // Урок 5. Функции и одномерные массивы
 
 // Рекомендуемая литература:
 // Книга, которая помогает пройти собеседование на C# Junior:
 // «CLR via C#. Программирование на платформе Microsoft .NET», Рихтер Дж.
 
+// *******************************************************************************
+
+// Задача 31: Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. 
+//Найдите сумму отрицательных и положительных элементов массива.
+// Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма положительных чисел равна 29, сумма отрицательных равна -20.
+
+// void InputArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//         array[i] = new Random().Next(-9, 10); // [-9, 9]
+// }
+
+
+// void ReleaseArray(int[] array)
+// {
+//     int sumPositive = 0, sumNegative = 0;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i] > 0)
+//             sumPositive += array[i];
+//         else
+//             sumNegative += array[i];
+//     }
+//     Console.WriteLine($"Сумма положительных чисел равна {sumPositive}");
+//     Console.WriteLine($"Сумма отрицательных чисел равна {sumNegative}");
+// }
+
+
+// Console.Clear();
+// Console.Write("Введите кол-во элементов массива: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int[] array = new int[n];
+// InputArray(array);
+// Console.WriteLine($"[{string.Join(", ", array)}]");
+// ReleaseArray(array);
+
+// *******************************************************************************
+
+// Задача 32: Напишите программу замена элементов массива: 
+// положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+// void InputArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//         array[i] = new Random().Next(-9, 10); // [-9, 9]
+// }
+
+
+// void ReleaseArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//         array[i] *= (-1);
+// }
+
+
+// Console.Clear();
+// Console.Write("Введите кол-во элементов массива: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int[] array = new int[n];
+// InputArray(array);
+// Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+// ReleaseArray(array);
+// Console.WriteLine($"Конечный массив: [{string.Join(", ", array)}]");
+
+
+// *******************************************************************************
+
+// Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, 3] -> да
+
+// void InputArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//         array[i] = new Random().Next(-9, 10); // [-9, 9]
+// }
+
+
+// string ReleaseArray(int[] array, int k)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i] == k)
+//             return "yes";
+//     }
+//     return "no";
+// }
+
+
+// Console.Clear();
+// Console.Write("Введите кол-во элементов массива: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int[] array = new int[n];
+// InputArray(array);
+// Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+// Console.Write("Введите число: ");
+// int k = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(ReleaseArray(array, k));
+
+// *******************************************************************************
+
+// Задача 35: Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива, значения которых лежат в отрезке [10,99]. 
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(1, 11); // [1, 10]
+}
+
+
+void ReleaseArray(int[] array)
+{
+    for (int i = 0; i < array.Length / 2 + array.Length % 2; i++)
+        Console.Write($"{array[i] * array[array.Length - 1 - i]} ");
+
+}
+
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+ReleaseArray(array);
+
+
+// *******************************************************************************
+
+// Задача 37: Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 3
+// [6 7 3 6] -> 36 21
+
+// void InputArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//         array[i] = new Random().Next(1, 11); // [1, 10]
+// }
+
+
+// int[] ReleaseArray(int[] array)
+// {
+//     int[] result = new int[array.Length / 2 + array.Length % 2];
+//     for (int i = 0; i < array.Length / 2 + array.Length % 2; i++)
+//         result[i] = array[i] * array[array.Length - 1 - i];
+//     return result;
+
+// }
+
+
+// Console.Clear();
+// Console.Write("Введите кол-во элементов массива: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int[] array = new int[n];
+// InputArray(array);
+// Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+// Console.WriteLine($"Конечный массив: [{string.Join(", ", ReleaseArray(array))}]");
