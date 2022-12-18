1655df06e9c89159c1e7b0200a86b14e6fa005ca
lenaruch <89196825388@mail.ru>
2022-12-19 01:37:23 +0300

    Task 41 modifide

--- Experements/Program.cs
+++ Experements/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿Console.Clear();
+Console.Write("Введите X первой точки: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите Y первой точки: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите X второй точки: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите Y второй точки: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+
+double dist1 = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
+double dist2 = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
+double dist3 = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
+
+Console.WriteLine(Math.Round(dist, 2));
--- Tasks/Task_41/Program.cs
+++ Tasks/Task_41/Program.cs
@@ -2,21 +2,37 @@
 // 0, 7, 8, -2, -2 -> 2
 // -1, -7, 567, 89, 223-> 3
 
+// Console.Clear();
+// Console.Write("Введите кол-во чисел: ");
+// int ElementsCount = Convert.ToInt32(Console.ReadLine());
+// int[] myArray = new int[ElementsCount];
+// int count = 0;
+
+// for (int i = 0; i < myArray.Length; i++)
+// {
+//     myArray[i] = Convert.ToInt32(Console.ReadLine());
+// }
+
+// for (int i = 0; i < myArray.Length; i++)
+// {
+//     if (myArray[i] > 0)
+//         count = count + 1;
+// }
+
+// Console.WriteLine(count);
+
+// Вариант, где пользователя не спрашивают количество элементов
+// а сразу указывают, что нужно указать данные через пробел
+
 Console.Clear();
-Console.Write("Введите кол-во чисел: ");
-int ElementsCount = Convert.ToInt32(Console.ReadLine());
-int[] myArray = new int[ElementsCount];
+Console.Write("Введите числа через пробел: ");
+int[] s = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
 int count = 0;
 
-for (int i = 0; i < myArray.Length; i++)
+for (int i = 0; i < s.Length; i++)
 {
-    myArray[i] = Convert.ToInt32(Console.ReadLine());
-}
-
-for (int i = 0; i < myArray.Length; i++)
-{
-    if (myArray[i] > 0)
+    if (s[i] > 0)
         count = count + 1;
 }
 
-Console.WriteLine(count);
+Console.WriteLine($"Введено положительных чисел: {count}");
