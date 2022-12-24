012d680e938add2730e621481281e9754a8f032c
lenaruch <89196825388@mail.ru>
2022-12-24 23:36:14 +0300

    Task 47 modified

--- Experements/Program.cs
+++ Experements/Program.cs
@@ -1,27 +1,41 @@
-﻿Console.Clear();
-Random random = new Random();
-int[,] arr = new int[random.Next(1, 10), random.Next(1, 10)];
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1 7 -> такой позиции в массиве нет (пользователь вводит нумерацию с 1)
 
-for (int i = 0; i < arr.GetLength(0); i++)
+void InputMatrix(int[,] matrix)
 {
-    for (int j = 0; j < arr.GetLength(1); j++)
+    for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        arr[i, j] = random.Next(1, 10);
-        Console.Write(arr[i, j] + " ");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(-10, 11); // [-10, 10]
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
     }
-    Console.WriteLine();
 }
 
-Console.WriteLine("---------------------------");
-
-Console.WriteLine(arr.GetLength(0));
-for (int j = 0; j < arr.GetLength(1); j++)
+Console.Clear();
+int[,] matrix = new int[5, 5];
+InputMatrix(matrix);
+Console.Write("Введите номер строки: ");
+int x = Convert.ToInt32(Console.ReadLine());
+while (x < 1 || x > 5)
 {
-    double sum = 0;
-    for (int i = 0; i < arr.GetLength(0); i++)
-    {
-        sum += arr[i, j];
-    }
-    Console.Write($"{ sum / arr.GetLength(0)} ");
+   Console.Write("Такой сторки нет, повторите ввод cтроки: ");
+   x = Convert.ToInt32(Console.ReadLine());
 }
-Console.ReadLine();
+Console.Write("Введите номер столбца: ");
+
+int y = Convert.ToInt32(Console.ReadLine());
+while (y < 1 || y > 5)
+{
+   Console.Write("Такой столбца нет, повторите ввод cтолбца: ");
+   y = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine(matrix[x-1, y-1]);
--- Tasks/Task_47/Program.cs
+++ Tasks/Task_47/Program.cs
@@ -1,24 +1,41 @@
-﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
-// m = 3, n = 4.
-// 0,5 7 -2 -0,2
-// 1 -3,3 8 -9,9
-// 8 7,8 -7,1 9
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1 7 -> такой позиции в массиве нет (пользователь вводит нумерацию с 1)
 
-void InputMatrix(double[,] matrix) // функция для создания двумерного массива
+void InputMatrix(int[,] matrix)
 {
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            matrix[i, j] = Math.Round(new Random().NextDouble() * (50 - 10) - 10, 2); // [-10, 50]
-            Console.Write($"{matrix[i, j]} \t"); // \t - добавляет Тав между элементами массива для красоты отображения
+            matrix[i, j] = new Random().Next(-10, 11); // [-10, 10]
+            Console.Write($"{matrix[i, j]} \t");
         }
         Console.WriteLine();
     }
 }
 
 Console.Clear();
-Console.Write("Введите размеры матрицы: ");
-int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray(); // ввод в одну строку, разделитель пробел
-double[,] matrix = new double[size[0], size[1]];
+int[,] matrix = new int[5, 5];
 InputMatrix(matrix);
+Console.Write("Введите номер строки: ");
+int x = Convert.ToInt32(Console.ReadLine());
+while (x < 1 || x > 5)
+{
+   Console.Write("Такой сторки нет, повторите ввод cтроки: ");
+   x = Convert.ToInt32(Console.ReadLine());
+}
+Console.Write("Введите номер столбца: ");
+
+int y = Convert.ToInt32(Console.ReadLine());
+while (y < 1 || y > 5)
+{
+   Console.Write("Такой столбца нет, повторите ввод cтолбца: ");
+   y = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine(matrix[x-1, y-1]);
